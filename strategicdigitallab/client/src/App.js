d19405cd3894571104a9a7b0b38d9e02a1c5de8f
naveena1111 <naveenz111194@gmail.com>
2019-11-05 18:00:35 -0800

    UI changes commit

--- strategicdigitallab/client/src/App.js
+++ strategicdigitallab/client/src/App.js
@@ -4,6 +4,7 @@ import { Route, Link, Router } from 'react-router-dom';
 import ContactUsComponent from './components/contactUs/contactUs.jsx';
 import Home from './components/home/home';
 import { history } from './helpers/history';
+// import './../img';
 
 class App extends React.Component {
 
@@ -22,9 +23,29 @@ class App extends React.Component {
 
 
                 <div>
-                   
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <img src={require('./img/logo1.png')} />
+     
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav> */}
+                    {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                  <img src={require('./img/logo1.png')} />
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav ml-auto">
                                 <li className="nav-item">
@@ -38,7 +59,31 @@ class App extends React.Component {
                                 </li>
                             </ul>
                         </div>
-                    </nav>
+                    </nav> */}
+
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <img src={require('./img/logo1.png')} />
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+       </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+               <li class="nav-item letterSpace-2 mr-4 ">
+               <Link to="/home" className="nav-link font-weight-bold ">ABOUT US</Link>                   {/* <a class="nav-link" href="#">ABOUT US <span class="sr-only">(current)</span></a> */}
+                 </li>
+                <li class="nav-item letterSpace-2 mr-4">
+                    <a class="nav-link  font-weight-bold" href="#">SERVICES</a>
+                 </li>
+                <li class="nav-item letterSpace-2 mr-4">
+                    <a class="nav-link  font-weight-bold" href="#">CASE STUDIES</a>
+                </li>
+                <li class="nav-item letterSpace-2 mr-3 ">
+                 <Link to="/" className="nav-link font-weight-bold active text-warning">CONTACT US</Link>
+                 </li>
+             </ul>
+         </div>
+     </nav>
                 </div>
 
               
